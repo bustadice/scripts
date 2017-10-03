f62f61c7274ac993ccdc28247a19d253efa27fb2
Daniel Evans <daniel@bustadice.com>
2017-10-02 15:48:51 -2200

    turboskip strategy

---
+++ turboskip.js
@@ -0,0 +1,14 @@
+// WARNING: This script is not throttled in any way. There is a good chance that
+// your browser will freeze and that you won't be able to stop the script until
+// all bets have been made.
+//
+// Assume that all bets will be placed and only run this script if you accept
+// that!
+
+
+const betCount = 100 // number of bets to skip
+
+
+for (let i = 0; i < betCount; i++) {
+  this.skip().catch(this.stop)
+}
