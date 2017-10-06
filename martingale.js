8219ee6662f60ed80f0360d7b366f6ce521753d4
Daniel Evans <daniel@bustadice.com>
2017-10-06 08:59:21 -0900

    improve martingale

--- martingale.js
+++ martingale.js
@@ -3,19 +3,23 @@ const target = 2.00     // target multiplier
 const betMultiplier = 2 // what to multiply the bet size by when we lose a wager
 
 
-let betSize = baseBet
+let lossCount = 0
 this.log(`Starting martingale with a base bet of ${baseBet} satoshis.`)
 
 while (true) {
   // make the bet and wait for the result
-  const { multiplier } = await this.bet(betSize, target)
+  const { multiplier } = await this.bet(betSize(lossCount), target)
 
   if (multiplier < target) { // loss
-    betSize *= betMultiplier
-    betSize = Math.round(betSize / 100) * 100 // round bet to nearest valid one
-    this.log(`Lost bet. Multiplying bet size by ${betMultiplier} for new bet size of ${betSize} satoshis.`)
+    lossCount++
+    this.log(`Lost bet. Multiplying bet size by ${betMultiplier} for new bet size of ${betSize(lossCount)} satoshis.`)
   } else { // win
-    betSize = baseBet
+    lossCount = 0
     this.log(`Won bet. Setting bet size to ${baseBet} satoshis.`)
   }
+}
+
+function betSize(lossCount) {
+  const bet = baseBet * Math.pow(betMultiplier, lossCount)
+  return Math.round(bet / 100) * 100
 }
